e67cc009489dadd7c2be3acb78d8096f7761231f
Jeff Zohrab <jzohrab@gmail.com>
2023-07-27 21:16:13 -0700

    wip

--- tests/src/Domain/TokenCoverage_Test.php
+++ tests/src/Domain/TokenCoverage_Test.php
@@ -4,6 +4,7 @@ require_once __DIR__ . '/../../db_helpers.php';
 require_once __DIR__ . '/../../DatabaseTestBase.php';
 
 use App\Entity\Book;
+use App\Entity\Term;
 use App\Domain\BookStats;
 use App\Domain\TermService;
 use App\Domain\ReadingFacade;
@@ -17,14 +18,34 @@ final class TokenCoverage_Test extends DatabaseTestBase
         $this->load_languages();
     }
 
-    public function test_stats_calculates_rendered_text() {
-        $t = $this->make_text("Hola.", "Tengo un gato.  Tengo un perro.", $this->spanish);
+    public function addTerm(string $s, int $status) {
+        $term_svc = new TermService($this->term_repo);
+        $term = new Term($this->spanish, $s);
+        $term->setStatus($status);
+        $term_svc->add($term, true);
+    }
+
+    public function scenario(string $fulltext, $terms_and_statuses) {
+        $t = $this->make_text("Hola.", $fulltext, $this->spanish);
         $b = $t->getBook();
-        $this->addTerms($this->spanish, [ "tengo un" ]);
+
+        foreach ($terms_and_statuses as $ts)
+            $this->addTerm($ts[0], $ts[1]);
 
         $tc = new TokenCoverage();
         $stats = $tc->getStats($b);
         dump($stats);
     }
+
+    public function test_single_word() {
+        $this->scenario("Tengo un gato.  Tengo un perro.",
+                        [[ "gato", 1 ]]);
+    }
+
+    public function test_with_multiword() {
+        $this->scenario("Tengo un gato.  Tengo un perro.",
+                        [[ "tengo un", 1 ]]);
+    }
+
 
 }
